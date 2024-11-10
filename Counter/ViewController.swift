d181d44f5d81385373de7d4ef5bc8baaf32997c8
Tenzin S <headlinerten@gmail.com>
2024-11-10 17:48:47 -0500

    Сounter functionality and UI updates added

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Переменная для хранения значения счетчика
+    var counterValue = 0
+    
+    // Outlet для лейбла
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    // Action для кнопки
+    @IBAction func incrementCounter(_ sender: Any) {
+        // Увеличиваем значение счетчика
+        counterValue += 1
+        // Обновляем текст лейбла
+        counterLabel.text = "Значение счетчика: \(counterValue)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Инициализация начального значения счетчика
+        counterLabel.text = "Значение счетчика: 0"
     }
 
 
